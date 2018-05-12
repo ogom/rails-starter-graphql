de109000feece6dcf48f03911bce640da2b920ec
ogom <ogom@outlook.com>
2018-05-12 15:26:46 +0900

    Generate scaffold Post and Comment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,12 @@
-# Be sure to restart your server when you modify this file.
+if Rails.env.development? || Rails.env.test?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins(/localhost\:\d+/)
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+      resource '*',
+               credentials: true,
+               headers: :any,
+               methods: %i[get post put patch delete options head]
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+1.upto(4) do |num|
+  post = Post.create_with(body: "body:#{num}").find_or_create_by(title: "title:#{num}")
+  1.upto(2) do |num|
+    post.comments.find_or_create_by(body: "post:#{post.id} body:#{num}")
+  end
+end
